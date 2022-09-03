d1628b157d89190dec97526c860cb2814a89e923
Andry <andrysitrakiniainasoa@gmail.com>
2022-09-03 19:05:50 +0300

    add app layout

---
+++ src/components/Left/index.jsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+import Logo from "./components/Logo";
+
+const Left = () => {
+  return (
+    <Box>
+      <Logo />
+    </Box>
+  );
+};
+
+export default Left;
