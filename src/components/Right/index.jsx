adacd09626ac4fd7b48666ba8eb7f9993b0b9c76
Andry <andrysitrakiniainasoa@gmail.com>
2022-09-04 16:34:46 +0300

    fix: remove unused variable in Right

--- src/components/Right/index.jsx
+++ src/components/Right/index.jsx
@@ -1,20 +1,16 @@
-import * as React from 'react';
-import AccountMenu from './Menu/Menu';
-import RightMenu from './Menu/Menu2';
-import SideContent from './SideContent/SideContent';
-import SocialMedia from './SocialMedia/SocialMedia';
-
-
+import * as React from "react";
+import RightMenu from "./Menu/Menu2";
+import SideContent from "./SideContent/SideContent";
+import SocialMedia from "./SocialMedia/SocialMedia";
 
 const Right = () => {
   return (
-  <div>
-    <RightMenu/>
-    {/* <AccountMenu/> */}
-    <SideContent/>
-    <SocialMedia/>
-  </div>
-  )
+    <div>
+      <RightMenu />
+      <SideContent />
+      <SocialMedia />
+    </div>
+  );
 };
 
 export default Right;
