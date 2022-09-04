4d476400d309b099a2539442a263a643159baf76
Andry <andrysitrakiniainasoa@gmail.com>
2022-09-04 15:59:16 +0300

    fix: set fixed width in AppLayout

--- src/layout/AppLayout/index.jsx
+++ src/layout/AppLayout/index.jsx
@@ -1,11 +1,14 @@
 import { Box, CssBaseline } from "@mui/material";
 import Grid from "@mui/material/Grid";
 
-const AppLayout = ({ leftComponent, rightComponent, children }) => {
+const AppLayout = ({ leftComponent, rightComponent }) => {
   return (
-    <Box sx={{ px: 8 }}>
+    <Box sx={{ px: 8, minWidth: "max-content" }}>
       <CssBaseline />
-      <Grid container>
+      <Grid
+        container
+        sx={{ minWidth: "1200px", maxWidth: "1200px", mx: "auto" }}
+      >
         <Grid item xs={6}>
           {leftComponent}
         </Grid>
