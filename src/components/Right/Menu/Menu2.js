defcb7566c4651ec6212d8cb92f4f321cbaab44d
Andry <andrysitrakiniainasoa@gmail.com>
2022-09-04 18:12:47 +0300

    fix background color and header width in Right component

--- src/components/Right/Menu/Menu2.js
+++ src/components/Right/Menu/Menu2.js
@@ -1,20 +1,19 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import ArrowForwardOutlinedIcon from '@mui/icons-material/ArrowForwardOutlined';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import ArrowForwardOutlinedIcon from "@mui/icons-material/ArrowForwardOutlined";
 
-
-const courseLab = ['course1', 'course2', 'course3', 'course4'];
+const courseLab = ["course1", "course2", "course3", "course4"];
 
 const RightMenu = () => {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
@@ -35,15 +34,23 @@ const RightMenu = () => {
     setAnchorElUser(null);
   };
 
-
-
-  
   return (
-    <AppBar position="static" sx={{ backgroundColor: '#cccccc16', width: '80%', marginLeft: '18%', boxShadow:'none'}}>
+    <AppBar
+      position="static"
+      sx={{
+        backgroundColor: "#cccccc16",
+        marginLeft: "18%",
+        boxShadow: "none",
+      }}
+    >
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-
-          <Box sx={{ flexGrow: 1 , display: { xs: 'flex', md: 'none' , color: 'blue'} }}>
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: { xs: "flex", md: "none", color: "blue" },
+            }}
+          >
             <IconButton
               size="large"
               aria-label="account of current user"
@@ -51,7 +58,7 @@ const RightMenu = () => {
               aria-haspopup="true"
               onClick={handleOpenNavMenu}
               color="inherit"
-              sx={{color: 'white'}}
+              sx={{ color: "white" }}
             >
               <MenuIcon />
             </IconButton>
@@ -59,99 +66,110 @@ const RightMenu = () => {
               id="menu-appbar"
               anchorEl={anchorElNav}
               anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
+                vertical: "bottom",
+                horizontal: "left",
               }}
               keepMounted
               transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
+                vertical: "top",
+                horizontal: "left",
               }}
               open={Boolean(anchorElNav)}
               onClose={handleCloseNavMenu}
               sx={{
-                display: { xs: 'block', md: 'none' },
+                display: { xs: "block", md: "none" },
               }}
             >
-                <MenuItem  onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">Home</Typography>
-                </MenuItem>
-                <MenuItem  onClick={handleCloseNavMenu}>
-                  <Typography onClick={handleOpenUserMenu} sx={{ my: 2, color: 'black', display: 'block' }} textAlign="center">Courses</Typography>
-                </MenuItem>
-                <MenuItem  onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">Blog</Typography>
-                </MenuItem>
-                <MenuItem  onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">Pricing</Typography>
-                </MenuItem>
-                <MenuItem  onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">Sign in</Typography>
-                </MenuItem>
+              <MenuItem onClick={handleCloseNavMenu}>
+                <Typography textAlign="center">Home</Typography>
+              </MenuItem>
+              <MenuItem onClick={handleCloseNavMenu}>
+                <Typography
+                  onClick={handleOpenUserMenu}
+                  sx={{ my: 2, color: "black", display: "block" }}
+                  textAlign="center"
+                >
+                  Courses
+                </Typography>
+              </MenuItem>
+              <MenuItem onClick={handleCloseNavMenu}>
+                <Typography textAlign="center">Blog</Typography>
+              </MenuItem>
+              <MenuItem onClick={handleCloseNavMenu}>
+                <Typography textAlign="center">Pricing</Typography>
+              </MenuItem>
+              <MenuItem onClick={handleCloseNavMenu}>
+                <Typography textAlign="center">Sign in</Typography>
+              </MenuItem>
             </Menu>
           </Box>
-        
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            
+
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            <Button
+              onClick={handleCloseNavMenu}
+              sx={{ my: 2, color: "white", display: "block" }}
+            >
+              Home
+            </Button>
+
+            <Tooltip title="Open courses">
               <Button
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}
+                onClick={handleOpenUserMenu}
+                sx={{ my: 2, color: "white", display: "block" }}
               >
-                Home
+                Courses{" "}
+                <ExpandMoreIcon sx={{ color: "white", fontSize: "100%" }} />
               </Button>
+            </Tooltip>
 
-              <Tooltip title="Open courses">
-          
-        <Button onClick={handleOpenUserMenu} sx={{ my: 2, color: 'white', display: 'block' }}>
-         Courses <ExpandMoreIcon sx={{ color: 'white' ,fontSize: '100%'}}/>
-        </Button>
-
-                  
-      </Tooltip>
-     
-      <Menu
-        sx={{ mt: '45px' }}
-        id="menu-appbar"
-        anchorEl={anchorElUser}
-        anchorOrigin={{
-          vertical: 'top',
-          horizontal: 'right',
-        }}
-        keepMounted
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: 'right',
-        }}
-        open={Boolean(anchorElUser)}
-        onClose={handleCloseUserMenu}
-      >
-        {courseLab.map((courseLab) => (
-          <MenuItem key={courseLab} onClick={handleCloseUserMenu}>
-            <Typography textAlign="center">{courseLab}</Typography>
-          </MenuItem>
-        ))}
-      </Menu>
-      <Button
-        onClick={handleCloseNavMenu}
-        sx={{ my: 2, color: 'white', display: 'block' }}
-        >
-        Blog
-      </Button>
-      <Button
-        onClick={handleCloseNavMenu}
-        sx={{ my: 2, color: 'white', display: 'block' }}
-        >
-        Pricing
-      </Button>
-      <Button
-        onClick={handleCloseNavMenu}
-        sx={{ my: 2, color: 'white', display: 'block' , marginLeft: '10%'}}
-        >
-        Sign in 
-            <ArrowForwardOutlinedIcon sx={{ color: 'white' ,fontSize: '90%'}}/>
-      </Button>
-              
-         
+            <Menu
+              sx={{ mt: "45px" }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {courseLab.map((courseLab) => (
+                <MenuItem key={courseLab} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{courseLab}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+            <Button
+              onClick={handleCloseNavMenu}
+              sx={{ my: 2, color: "white", display: "block" }}
+            >
+              Blog
+            </Button>
+            <Button
+              onClick={handleCloseNavMenu}
+              sx={{ my: 2, color: "white", display: "block" }}
+            >
+              Pricing
+            </Button>
+            <Button
+              onClick={handleCloseNavMenu}
+              sx={{
+                my: 2,
+                color: "white",
+                display: "block",
+                marginLeft: "10%",
+              }}
+            >
+              Sign in
+              <ArrowForwardOutlinedIcon
+                sx={{ color: "white", fontSize: "90%" }}
+              />
+            </Button>
           </Box>
         </Toolbar>
       </Container>
@@ -159,4 +177,3 @@ const RightMenu = () => {
   );
 };
 export default RightMenu;
-
